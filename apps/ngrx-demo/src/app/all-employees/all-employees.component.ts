c23a9025028e9ba85ddacdf6e406bf277b49f28b
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-06-27 19:14:28 -0400

    chore: updates all employees comp

--- apps/ngrx-demo/src/app/all-employees/all-employees.component.ts
+++ apps/ngrx-demo/src/app/all-employees/all-employees.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Employee } from '../employee/employee';
 import { EmployeeService } from '../employee/employee.service';
 import { Observable } from 'rxjs';
-import { EntityCollectionServiceFactory } from '@ngrx/data';
 
 @Component({
 	selector: 'nx-projects-all-employees',
@@ -12,17 +11,11 @@ import { EntityCollectionServiceFactory } from '@ngrx/data';
 	templateUrl: './all-employees.component.html',
 	styleUrls: ['./all-employees.component.scss'],
 })
-export class AllEmployeesComponent implements OnInit{
+export class AllEmployeesComponent {
 	employees$: Observable<Employee[]> | undefined;
-	//employeeService2: EntityCollectionService<Employee>;
 
-	constructor(private employeeService: EmployeeService,entityCollectionServiceFactory: EntityCollectionServiceFactory) {
-		this.employees$ = this.employeeService.entities$;
-		//this.employeeService2 = entityCollectionServiceFactory.create<Employee>("Employee");
+	constructor(private employeeService: EmployeeService) {
+		this.employees$ = this.employeeService.getAll();
 		this.employeeService.entities$.subscribe((data) => console.log(data));
-	}
-
-	ngOnInit(): void {
-		this.employees$=this.employeeService.entities$;
 	}
 }
